bb64ea47978f573f9753e10776b08de599068e3f
Nicholas <nicho.tjornelund@gmail.com>
2018-11-15 15:42:43 +0100

    tinyC

--- src/main.js
+++ src/main.js
@@ -1,18 +1,3 @@
-var fs      = require("fs")
-var ASTY    = require("asty")
-var PEG     = require("pegjs")
-var PEGUtil = require("pegjs-util")
+const grammar = require("./grammar.js");
 
-var asty = new ASTY()
-var parser = PEG.generate(fs.readFileSync("syntax.pegjs", "utf8"))
-var result = PEGUtil.parse(parser, "hello,hey", {
-    startRule: "start",
-    makeAST: function (line, column, offset, args) {
-        return asty.create.apply(asty, args).pos(line, column, offset)
-    }
-})
-if (result.error !== null)
-    console.log("ERROR: Parsing Failure:\n" +
-        PEGUtil.errorMessage(result.error, true).replace(/^/mg, "ERROR: "))
-else
-    console.log(result.ast.dump().replace(/\n$/, ""))
+console.log(grammar("if(2){4+5;}else{1+1;}"))
