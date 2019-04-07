c534a65fe2706f34756c7953d450c8acedc6830c
Nicholas <nicho.tjornelund@gmail.com>
2019-04-07 10:52:55 +0200

    a star

---
+++ src/Graph.js
@@ -0,0 +1,15 @@
+class Graph {
+    constructor (){
+
+    }
+
+    adj(u) {
+        return "todo"
+    }
+    dist(u,v) {
+        return "todo"
+    }
+    coords(u) {
+        return [0,0]
+    }
+}
--- src/algorithms.js
+++ src/algorithms.js
@@ -1,25 +1,64 @@
-function dijkstra (G, s, t=null) {
-	var Q = queue()
-	Q.add(s,0)
-	var dist = {}
-	var prev = {}
-	dist[s] = 0
+function dijkstra(G, s, t = null) {
+    var Q = new PriorityQueue();
+    Q.push(s, 0);
+    var dist = {};
+    var prev = {};
+    dist[s] = 0;
 
-	
+    while (!Q.size()) {
+        var u = Q.pop();
 
-	while(!Q.size()){
-		var u = Q.pop()
+        if (t != null && u == t) {
+            return
+        }
 
-		if (t != null && u == t) {
-			return 
-		}
+        for (v in G.adj[u]) {
 
-		for (v in G[u])) {
-			var alt = dist(v)+dist(u,v)
-			if (alt < dist(v)) {
-				dist[v] = alt
-				prev[v] = u
-			}
-		}
-	}
+            d = (dist[v] == undefined) ? Infinity : dist[v];
+            var alt = dist[u] + G.dist(u, v)
+            if (alt < d) {
+                dist[v] = alt
+                prev[v] = u
+                q.push(v, alt)
+            }
+        }
+    }
+}
+
+function eucledian(a, b) {
+    [x1, y1] = a
+        [x2, y2] = b
+    return Math.sqrt(Math.pow(x1 - x2, 2), Math.pow(y1 - y2, 2))
+}
+
+function Astar(G, s, t, pi =) {
+    var Q = new PriorityQueue();
+    Q.push(s, 0);
+    var dist = {};
+    var prev = {};
+    var visited = {};
+    dist[s] = 0;
+
+    while (!Q.size()) {
+        var u = Q.pop();
+
+        if (u == t) {
+            return
+        }
+
+        visited[u] = true;
+
+        G.adj[u].forEach(function (e) {
+            if (visited[v] == true)
+                return
+
+            d = (dist[v] == undefined) ? Infinity : dist[v];
+            var alt = dist[u] + G.dist(u, v)
+            if (alt < d) {
+                dist[v] = alt
+                prev[v] = u
+                q.push(v, alt+pi(v,t))
+            }
+        })
+    }
 }
